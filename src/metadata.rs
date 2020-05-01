mechiru/gcemeta

commit 2a9ef1987d4542b389f5ffcf4f23174718672b21

remove clippy warnings

src/metadata.rs
@@ -262,20 +262,20 @@ fn json_array<S: AsRef<str>>(s: S) -> Result<Vec<String>> {
 
 fn parse_instance_name<S: AsRef<str>>(s: S) -> Result<String> {
     s.as_ref()
-        .split(".")
+        .split('.')
         .next()
         .filter(|x| !x.is_empty())
-        .map(|x| x.into())
-        .ok_or(ErrorKind::MetadataParse("instance name").into())
+        .map(Into::into)
+        .ok_or_else(|| ErrorKind::MetadataParse("instance name").into())
 }
 
 fn parse_zone<S: AsRef<str>>(s: S) -> Result<String> {
     s.as_ref()
-        .split("/")
+        .split('.')
         .last()
         .filter(|x| !x.is_empty())
-        .map(|x| x.into())
-        .ok_or(ErrorKind::MetadataParse("zone").into())
+        .map(Into::into)
+        .ok_or_else(|| ErrorKind::MetadataParse("zone").into())
 }
 
 #[cfg(test)]

commit 5a7da2dc0dc400db9cfc045b23bc821cb9e057a5

fix bug caused by 2a9ef1987d4542b389f5ffcf4f23174718672b21

src/metadata.rs
@@ -271,7 +271,7 @@ fn parse_instance_name<S: AsRef<str>>(s: S) -> Result<String> {
 
 fn parse_zone<S: AsRef<str>>(s: S) -> Result<String> {
     s.as_ref()
-        .split('.')
+        .split('/')
         .last()
         .filter(|x| !x.is_empty())
         .map(Into::into)